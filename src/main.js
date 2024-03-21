83f764b997db64d65c623e2618571460d4cadf4f
dev.ckh <ckyuhwan1130@naver.com>
2024-03-21 17:57:32 +0900

    20240321) createWindow

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 console.log("Starting main.js");
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const Win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    Win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if(ProcessingInstruction.platform !== "darwin"){
+        app.quit();
+    }
+});
