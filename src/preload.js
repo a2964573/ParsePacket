67527a17ebd5b124e118db925b8721d54ad0f352
dev.ckh <ckyuhwan1130@naver.com>
2024-03-21 17:28:55 +0900

    20240321) reset

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require("electron");
-
-console.log("Starting Preload.js");
-
-contextBridge.exposeInMainWorld("api", {
-    print: (message) => {
-        console.log(message);
-    }
-});
